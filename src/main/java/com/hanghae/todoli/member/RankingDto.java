035ee433b51fe05f0c917ff3c42cdc2484084a45
junsj119 <wjstjddud119@naver.com>
2022-07-09 19:33:29 +0900

    ranking push

---
+++ src/main/java/com/hanghae/todoli/member/RankingDto.java
@@ -0,0 +1,16 @@
+package com.hanghae.todoli.member;
+
+import com.hanghae.todoli.character.Dto.ThumbnailDto;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class RankingDto {
+    private Long memberId;
+    private String nickname;
+    private String thumbnailCharImg;
+    private List<ThumbnailDto> equipItems;
+}
