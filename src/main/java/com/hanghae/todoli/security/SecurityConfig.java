517cab630490c6a030acc30ff5b7df7896cf2ba0
junsj119 <wjstjddud119@naver.com>
2022-07-13 02:18:32 +0900

    zsc

--- src/main/java/com/hanghae/todoli/security/SecurityConfig.java
+++ src/main/java/com/hanghae/todoli/security/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/users/**").permitAll()
                 .antMatchers("/api/login/**").permitAll()
                 .antMatchers("/h2-console/**").permitAll()
-                .antMatchers("/health").permitAll()
+                .antMatchers("/").permitAll()
                 // 그 외 모든 요청은 인증과정 필요
                 .anyRequest().authenticated()
                  .and()
