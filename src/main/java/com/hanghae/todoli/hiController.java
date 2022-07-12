9e9318c891504b7560cc72b5d76507ab90a32d7f
junsj119 <wjstjddud119@naver.com>
2022-07-13 02:23:08 +0900

    aa

--- src/main/java/com/hanghae/todoli/hiController.java
+++ src/main/java/com/hanghae/todoli/hiController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class hiController {
     @GetMapping("/")
     public String abc(){
-        return "home";
+        return "OKOKOKOKOKOKOK";
     }
 }
