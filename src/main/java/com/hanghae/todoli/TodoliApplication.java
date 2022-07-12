a04c4d29aacb1676799ec1764ee0302597546d42
hyeongjun-Ham <so23go@naver.com>
2022-07-12 12:09:33 +0900

    -test4

--- src/main/java/com/hanghae/todoli/TodoliApplication.java
+++ src/main/java/com/hanghae/todoli/TodoliApplication.java
@@ -9,7 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class TodoliApplication {
-    //테스트1
+    //테스트3
     public static void main(String[] args) {
         SpringApplication.run(TodoliApplication.class, args);
     }
