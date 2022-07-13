34d33f0aff04caf93ad5fa549a59691a33e42c41
junsj119 <wjstjddud119@naver.com>
2022-07-13 13:47:38 +0900

    test

--- src/main/java/com/hanghae/todoli/hiController.java
+++ src/main/java/com/hanghae/todoli/hiController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class hiController {
     @GetMapping("/health")
     public String abc(){
-        return "rrrrrrrrrrr";
+        return "111111111111";
     }
 }
