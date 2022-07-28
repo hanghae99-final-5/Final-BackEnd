0142414ac843814da276a8ba34a4c6a92a6d9bea
hyeongjun-Ham <so23go@naver.com>
2022-07-28 10:54:02 +0900

    refactor(cicdCheckController) : rename
    컨트롤러 이름 변경

--- src/main/java/com/hanghae/todoli/CICDChenckController.java
+++ src/main/java/com/hanghae/todoli/CICDChenckController.java
@@ -1,11 +1,10 @@
 package com.hanghae.todoli;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class hiController {
+public class CICDChenckController {
     @GetMapping("/health")
     public String abc(){
         return "진짜 축하합니다!";
